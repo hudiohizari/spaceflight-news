b42e73fde6bb24ce967e3f9e25b38f7a89bd0062
hizari <hhizari@gmail.com>
2024-10-24 16:30:17 +0700

    fix: news section content padding

--- app/src/main/java/com/hizari/spaceflightnews/ui/component/news/NewsSection.kt
+++ app/src/main/java/com/hizari/spaceflightnews/ui/component/news/NewsSection.kt
@@ -4,17 +4,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -67,6 +70,7 @@ fun PreviewNewsSection() {
 @Composable
 fun NewsSection(
     modifier: Modifier = Modifier,
+    contentPadding: Int = 16,
     newsList: List<News>,
     onClickSeeMore: () -> Unit,
     textPadding: Int = 16,
@@ -102,6 +106,9 @@ fun NewsSection(
                 height = Dimension.value(120.dp)
                 width = Dimension.fillToConstraints
             },
+            contentPadding = PaddingValues(
+                horizontal = contentPadding.dp,
+            ),
             horizontalArrangement = Arrangement.spacedBy(12.dp),
         ) {
             items(newsList) { news ->
