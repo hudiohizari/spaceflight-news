27d5ae4b8a2134ca2c719144217edd2e3b9a3a94
hizari <hhizari@gmail.com>
2024-10-24 10:59:48 +0700

    feat: theme and typography

--- app/src/main/java/com/hizari/spaceflightnews/ui/theme/Color.kt
+++ app/src/main/java/com/hizari/spaceflightnews/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.hizari.spaceflightnews.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryBlue = Color(0xFF003D81)
+val SecondaryBlue = Color(0xFF3796FF)
+val LightBackground = Color.White
+val DarkBackground = Color(0xFF1C1C1C)
+val LightSurface = Color.White
+val DarkSurface = Color(0xFF2C2C2C)
+val OnPrimaryLight = Color.White
+val OnPrimaryDark = Color.White
+val OnSecondaryLight = Color.Black
+val OnSecondaryDark = Color.Black
+val OnBackgroundLight = Color.Black
+val OnBackgroundDark = Color.White
+val OnSurfaceLight = Color.Black
+val OnSurfaceDark = Color.White
+val TertiaryColor = Color(0xFFCCCCCC)
--- app/src/main/java/com/hizari/spaceflightnews/ui/theme/Type.kt
+++ app/src/main/java/com/hizari/spaceflightnews/ui/theme/Type.kt
@@ -4,31 +4,83 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.hizari.spaceflightnews.R
 
-// Set of Material typography styles to start with
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Manrope")
+
+val fontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.SemiBold),
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.ExtraBold),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = Typography().displayLarge.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    displayMedium = Typography().displayMedium.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    displaySmall = Typography().displaySmall.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    headlineLarge = Typography().headlineLarge.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    headlineMedium = Typography().headlineMedium.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    headlineSmall = Typography().headlineSmall.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    titleLarge = Typography().titleLarge.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    titleMedium = Typography().titleMedium.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    titleSmall = Typography().titleSmall.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    bodyLarge = Typography().bodyLarge.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    bodyMedium = Typography().bodyMedium.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    bodySmall = Typography().bodySmall.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    labelLarge = Typography().labelLarge.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    labelMedium = Typography().labelMedium.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    labelSmall = Typography().labelSmall.copy(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold
     )
-    */
 )
