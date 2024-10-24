3659bd60a7426d13802c12dda58e4af53696acd3
hizari <hhizari@gmail.com>
2024-10-24 11:07:01 +0700

    fix: greeting strings

--- app/src/main/java/com/hizari/spaceflightnews/ui/component/Greeting.kt
+++ app/src/main/java/com/hizari/spaceflightnews/ui/component/Greeting.kt
@@ -1,13 +1,14 @@
 package com.hizari.spaceflightnews.ui.component
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import com.hizari.spaceflightnews.R
 import com.hizari.spaceflightnews.ui.theme.SpaceflightNewsTheme
 import java.util.Calendar
 
@@ -37,9 +38,10 @@ fun Greeting(
     val currentHour = remember { Calendar.getInstance().get(Calendar.HOUR_OF_DAY) }
 
     val greeting = when (currentHour) {
-        in 0..11 -> "Good Morning"
-        in 12..17 -> "Good Evening"
-        else -> "Good Night"
+        in 0..11 -> stringResource(R.string.good_morning)
+        in 12..15 -> stringResource(R.string.good_afternoon)
+        in 16..19 -> stringResource(R.string.good_evening)
+        else -> stringResource(R.string.good_night)
     }
     Text(
         modifier = modifier,
